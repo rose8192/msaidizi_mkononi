66858e9d21b85922c7b8c3770f0455e48aaee33d
Your Name <your-email@example.com>
2026-02-14 12:43:30 +0300

    Fix_Android_settings_for_CI_environment

--- frontend/flutter_app/android/settings.gradle.kts
+++ frontend/flutter_app/android/settings.gradle.kts
@@ -2,11 +2,15 @@ pluginManagement {
     val flutterSdkPath =
         run {
             val properties = java.util.Properties()
-            file("local.properties").inputStream().use { properties.load(it) }
-            val flutterSdkPath = properties.getProperty("flutter.sdk")
-            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-            flutterSdkPath
+            val localProperties = file("local.properties")
+            if (localProperties.exists()) {
+                localProperties.inputStream().use { properties.load(it) }
+                properties.getProperty("flutter.sdk")
+            } else {
+                System.getenv("FLUTTER_ROOT")
+            }
         }
+    require(flutterSdkPath != null) { "flutter.sdk not set in local.properties or FLUTTER_ROOT environment variable" }
 
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
